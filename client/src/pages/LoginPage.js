964325dc12789442e058ae1d8b2797e2a2847271
Mirhadi Seyidli <Mirhadi.Seyidli@Mirhadis-MBP.attlocal.net>
2024-08-30 08:55:22 -0700

    nginx

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 function LoginPage() {
   const handleLogin = () => {
-    window.location.href = 'http://127.0.0.1:3050/api/auth/google';
+    window.location.href = '/api/auth/google';
   };
 
   return (
     <div className="login-page">
       <h2>Login to Drone Asset Management</h2>
+      <h1>Here</h1>
       <button onClick={handleLogin}>Login with Google</button>
     </div>
   );
